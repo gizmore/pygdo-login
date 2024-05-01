e509457b22763007775a0c0e5841c01f7dcc5b4d
gizmore <gizmore@wechall.net>
2024-05-01 15:55:45 +0200

    'math.sum'

--- GDT_Login.py
+++ GDT_Login.py
@@ -4,4 +4,4 @@ from gdo.core.GDT_String import GDT_String
 class GDT_Login(GDT_String):
 
     def __init__(self, name):
-        super(GDT_Login, self).__init__(name)
+        super().__init__(name)
