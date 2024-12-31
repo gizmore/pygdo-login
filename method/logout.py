2333ef243faa10e85b302f8a472bd486e254fcce
gizmore <gizmore@wechall.net>
2024-12-31 14:17:05 +0100

    'sync bs5'

--- method/logout.py
+++ method/logout.py
@@ -21,4 +21,4 @@ class logout(Method):
 
     def gdo_execute(self) -> GDT:
         self._env_user.logout(self._env_session)
-        return self.reply('msg_logged_out')
+        return self.msg('msg_logged_out')
